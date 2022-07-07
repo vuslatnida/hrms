733660e55dda42fdb04d174cf31b7deaf4f9e7c4
vuslatnida <nida.nylrm@gmail.com>
2022-07-07 08:50:10 +0300

    Hrms project

--- src/main/java/hrms/hrms/entities/concretes/dtos/PositionDto.java
+++
@@ -1,9 +0,0 @@
-package hrms.hrms.entities.concretes.dtos;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-@Data
-@AllArgsConstructor
-public class PositionDto {
-    private String job_position;
-}
