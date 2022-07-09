4e596a04465f627838608413f3629077ebad8725
vuslatnida <nida.nylrm@gmail.com>
2022-07-09 17:22:12 +0300

    Excel

--- src/main/java/hrms/hrms/core/utilities/excelHelper/EmployerListExcelHelper.java
+++ src/main/java/hrms/hrms/core/utilities/excelHelper/EmployerListExcelHelper.java
@@ -31,77 +31,77 @@ public class EmployerListExcelHelper {
         sheet = workbook.createSheet(SHEET);
     }
 
-        public void export(HttpServletResponse response) throws IOException {
+    public void export(HttpServletResponse response) throws IOException {
 
-            Row headerRow = sheet.createRow(0);
+        Row headerRow = sheet.createRow(0);
 
-            CellStyle headerCellStyle = workbook.createCellStyle();
-            XSSFFont headerFontStyle = (XSSFFont) workbook.createFont();
-            headerFontStyle.setBold(true);
-            headerFontStyle.setFontHeight(16);
-            headerCellStyle.setFont(headerFontStyle);
+        CellStyle headerCellStyle = workbook.createCellStyle();
+        XSSFFont headerFontStyle = (XSSFFont) workbook.createFont();
+        headerFontStyle.setBold(true);
+        headerFontStyle.setFontHeight(16);
+        headerCellStyle.setFont(headerFontStyle);
 
-            for (int col = 0; col < HEADERs.length; col++) {
-                Cell cell = headerRow.createCell(col);
-                cell.setCellValue(HEADERs[col]);
-                cell.setCellStyle(headerCellStyle);
-            }
-
-            int rowIdx = 1;
-            CellStyle rowCellStyle = workbook.createCellStyle();
-            XSSFFont rowFontStyle = (XSSFFont) workbook.createFont();
-            rowFontStyle.setFontHeight(13);
-            rowCellStyle.setFont(rowFontStyle);
-            rowFontStyle.setColor(IndexedColors.BLUE.getIndex());
-
-            for (Employer employee : employers) {
-                Row dataRow = sheet.createRow(rowIdx++);
-
-                Cell cell = dataRow.createCell(0);
-                cell.setCellValue(employee.getPerson().getFirstName());
-                sheet.autoSizeColumn(0);
-                cell.setCellStyle(rowCellStyle);
-
-                cell = dataRow.createCell(1);
-                cell.setCellValue(employee.getPerson().getLastName());
-                sheet.autoSizeColumn(1);
-                cell.setCellStyle(rowCellStyle);
-
-                cell = dataRow.createCell(2);
-                cell.setCellValue(employee.getCompanyName());
-                sheet.autoSizeColumn(2);
-                cell.setCellStyle(rowCellStyle);
-
-                cell = dataRow.createCell(3);
-                cell.setCellValue(employee.getWebsite());
-                sheet.autoSizeColumn(3);
-                cell.setCellStyle(rowCellStyle);
-
-                cell = dataRow.createCell(4);
-                cell.setCellValue(employee.getWebsiteMail());
-                sheet.autoSizeColumn(4);
-                cell.setCellStyle(rowCellStyle);
-
-                cell = dataRow.createCell(5);
-                cell.setCellValue(employee.getPhoneNo());
-                sheet.autoSizeColumn(5);
-                cell.setCellStyle(rowCellStyle);
-
-                cell = dataRow.createCell(6);
-                cell.setCellValue(employee.getPassword());
-                sheet.autoSizeColumn(6);
-                cell.setCellStyle(rowCellStyle);
-
-                cell = dataRow.createCell(7);
-                cell.setCellValue(employee.getSystemPersonnel().getJobposition());
-                sheet.autoSizeColumn(7);
-                cell.setCellStyle(rowCellStyle);
+        for (int col = 0; col < HEADERs.length; col++) {
+            Cell cell = headerRow.createCell(col);
+            cell.setCellValue(HEADERs[col]);
+            cell.setCellStyle(headerCellStyle);
         }
 
-            ServletOutputStream outputStream =  response.getOutputStream();
-            workbook.write(outputStream);
-            workbook.close();
-            outputStream.close();
+        int rowIdx = 1;
+        CellStyle rowCellStyle = workbook.createCellStyle();
+        XSSFFont rowFontStyle = (XSSFFont) workbook.createFont();
+        rowFontStyle.setFontHeight(13);
+        rowCellStyle.setFont(rowFontStyle);
+        rowFontStyle.setColor(IndexedColors.BLUE.getIndex());
+
+        for (Employer employee : employers) {
+            Row dataRow = sheet.createRow(rowIdx++);
+
+            Cell cell = dataRow.createCell(0);
+            cell.setCellValue(employee.getPerson().getFirstName());
+            sheet.autoSizeColumn(0);
+            cell.setCellStyle(rowCellStyle);
+
+            cell = dataRow.createCell(1);
+            cell.setCellValue(employee.getPerson().getLastName());
+            sheet.autoSizeColumn(1);
+            cell.setCellStyle(rowCellStyle);
+
+            cell = dataRow.createCell(2);
+            cell.setCellValue(employee.getCompanyName());
+            sheet.autoSizeColumn(2);
+            cell.setCellStyle(rowCellStyle);
+
+            cell = dataRow.createCell(3);
+            cell.setCellValue(employee.getWebsite());
+            sheet.autoSizeColumn(3);
+            cell.setCellStyle(rowCellStyle);
+
+            cell = dataRow.createCell(4);
+            cell.setCellValue(employee.getWebsiteMail());
+            sheet.autoSizeColumn(4);
+            cell.setCellStyle(rowCellStyle);
+
+            cell = dataRow.createCell(5);
+            cell.setCellValue(employee.getPhoneNo());
+            sheet.autoSizeColumn(5);
+            cell.setCellStyle(rowCellStyle);
+
+            cell = dataRow.createCell(6);
+            cell.setCellValue(employee.getPassword());
+            sheet.autoSizeColumn(6);
+            cell.setCellStyle(rowCellStyle);
+
+            cell = dataRow.createCell(7);
+            cell.setCellValue(employee.getSystemPersonnel().getJobposition());
+            sheet.autoSizeColumn(7);
+            cell.setCellStyle(rowCellStyle);
+        }
+
+        ServletOutputStream outputStream =  response.getOutputStream();
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
 
     }
 }
