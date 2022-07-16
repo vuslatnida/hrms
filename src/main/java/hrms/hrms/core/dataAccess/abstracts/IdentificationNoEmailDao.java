1f674f46eeb05da035d767fb93750b65704966b6
vuslatnida <nida.nylrm@gmail.com>
2022-07-16 20:49:40 +0300

    Hrms project

--- src/main/java/hrms/hrms/core/dataAccess/abstracts/IdentificationNoEmailDao.java
+++ src/main/java/hrms/hrms/core/dataAccess/abstracts/IdentificationNoEmailDao.java
@@ -4,6 +4,8 @@ import hrms.hrms.entities.concretes.JobSeekers;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IdentificationNoEmailDao extends JpaRepository<JobSeekers, String> {
+
     boolean existsByIdentificationNo(String identificationNo);
+
     boolean existsByEmail(String email);
 }
--- src/main/java/hrms/hrms/core/dataAccess/abstracts/WebMailDao.java
+++ src/main/java/hrms/hrms/core/dataAccess/abstracts/WebMailDao.java
@@ -4,5 +4,6 @@ import hrms.hrms.entities.concretes.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WebMailDao extends JpaRepository<Employer, String> {
+
     boolean existsByWebsiteMail(String websiteMail);
 }
